0481155bf933b8c9f53ceac0d0e37c15844320cf
Adrian-tabago <adrianchester.tabago04@gmail.com>
2026-05-07 20:48:55 +0800

    Add files via upload

---
+++ CaseStudy-main/CaseStudy-main/caseStudy/caseStudy/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackgroundImageLayout = ImageLayout.Stretch
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        If txtUsername.Text.Equals("") Or txtBoxPass.Text.Equals("") Then
+            MessageBox.Show("Please fill in all fields")
+        Else
+            Dim Status As New FormStatus
+            Dim frmComplaint As New Form2
+
+            Form2.Show()
+            Form2.lblName.Text = txtUsername.Text
+
+
+            Me.Hide()
+        End If
+
+    End Sub
+
+
+End Class
---
+++ CaseStudy-main/CaseStudy-main/caseStudy/caseStudy/Form2.vb
@@ -0,0 +1,66 @@
+﻿Public Class Form2
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        rdBtnOthers.Checked = True
+        'txtBoxComOthers.ReadOnly = True
+
+        If rdBtnOthers.Checked Then
+            txtBoxComOthers.ReadOnly = False
+        Else
+            txtBoxComOthers.ReadOnly = True
+        End If
+    End Sub
+
+    'Private Sub rdBtnOthers_CheckedChanged(sender As Object, e As EventArgs) Handles rdBtnOthers.CheckedChanged
+
+    '    If rdBtnOthers.Checked Then
+    '        txtBoxComOthers.ReadOnly = False
+    '    Else
+    '        txtBoxComOthers.Clear()
+    '    End If
+
+    'End Sub
+
+    Private Sub btnFComplaint_Click(sender As Object, e As EventArgs) Handles btnFComplaint.Click
+        Dim frmStatus As New FormStatus
+        Dim dateToday As Date = Date.Now
+
+
+        If txtBoxAddress.Text.Equals("") Or txtBoxContact.Text.Equals("") Or txtBoxDesc.Text.Equals("") Then
+            If txtBoxComOthers.Text.Equals("") Then
+                MessageBox.Show("Please fill in the empty fields")
+            End If
+        Else
+            If rdBtnOthers.Checked Then
+                FormStatus.dataStatus.Rows.Add(Form1.txtUsername.Text, txtBoxAddress.Text, txtBoxContact.Text, txtBoxComOthers.Text, "Pending", dateToday)
+            ElseIf rdBtnDispute.Checked Then
+                FormStatus.dataStatus.Rows.Add(Form1.txtUsername.Text, txtBoxAddress.Text, txtBoxContact.Text, "Dispute", "Pending", dateToday)
+
+            ElseIf rdNoise.Checked Then
+                FormStatus.dataStatus.Rows.Add(Form1.txtUsername.Text, txtBoxAddress.Text, txtBoxContact.Text, "Noise", "Pending", dateToday)
+
+            ElseIf rdInfrastructure.Checked Then
+                FormStatus.dataStatus.Rows.Add(Form1.txtUsername.Text, txtBoxAddress.Text, txtBoxContact.Text, "Infrastructure", "Pending", dateToday)
+            End If
+        End If
+
+    End Sub
+
+    Private Sub btnBack_Click(sender As Object, e As EventArgs)
+        Dim frmLogin As New Form1
+
+    End Sub
+
+    Private Sub btnComplain_MouseEnter(sender As Object, e As EventArgs) Handles btnComplain.MouseEnter
+        btnComplain.BackColor = Color.White
+    End Sub
+
+    Private Sub btnComplain_MouseLeave(sender As Object, e As EventArgs) Handles btnComplain.MouseLeave
+        btnComplain.BackColor = Color.White
+    End Sub
+
+    Private Sub btnComplain_Click(sender As Object, e As EventArgs) Handles btnComplain.Click
+        FormStatus.Show()
+        Me.Hide()
+    End Sub
+End Class
